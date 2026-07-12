f5fbbef94ba3d1b3c53d00392b5b7f2e17fea24f
garvey17 <adebolaoluwatowo@gmail.com>
2026-07-12 13:14:07 +0100

    docker compose updated

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    database_url: str = ""
-    openai_api_key:str = ""
-    pinecone_api_key:str = ""
-    pinecone_index_name:str = ""
+    database_url: str
+    openai_api_key: str
+    pinecone_api_key: str
+    pinecone_index_name: str
 
-    model_config = SettingsConfigDict(env_file=".env")
-
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
 settings = Settings()
